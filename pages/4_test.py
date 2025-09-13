c8c30bb4d36985f3240c1c9c3a6561cd2a94cd70
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-13 19:03:54 +0330

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -13,7 +13,6 @@ st.set_page_config(
 st.title("✨ITS Tokens")
 
 st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
-st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Sidebar Footer Slightly Left-Aligned ---
 st.sidebar.markdown(
